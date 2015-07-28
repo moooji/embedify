5a22bf25ac84cf4f49427be8900ce6144b7e5a39
moooji <steffen.straetz@gmail.com>
2015-07-28 11:36:18 +0200

    Improved plugin loading

--- plugins/youtube.js
+++ plugins/youtube.js
@@ -31,15 +31,15 @@ function get(url) {
 
 const tests = [
     {
-        content: '<iframe src="https://player.vimeo.com/video/132252780" width="600" height="338" frameborder="0" title="We Are Royale - Manifesto"></iframe>',
+        url: '<iframe src="https://player.vimeo.com/video/132252780" width="600" height="338" frameborder="0" title="We Are Royale - Manifesto"></iframe>',
         result: [{ id: "132252780", url: "https://player.vimeo.com/video/132252780"}]
     },
     {
-        content: '<a href="https://player.vimeo.com/video/132252780">Video</a>',
+        url: '<a href="https://player.vimeo.com/video/132252780">Video</a>',
         result: [{ id: "132252780", url: "https://player.vimeo.com/video/132252780"}]
     },
     {
-        content: '<a href="https://player.vimeo.com/video/132252780">Video</a><a href="https://player.vimeo.com/video/132252781">Video</a>',
+        url: '<a href="https://player.vimeo.com/video/132252780">Video</a><a href="https://player.vimeo.com/video/132252781">Video</a>',
         result: [
             {
                 id: "132252780",
@@ -51,7 +51,7 @@ const tests = [
             }]
     },
     {
-        content: '<a href="https://player.vimeo.com/video/132252780">Video</a><a href="https://www.vimeo.com/video/132252780">Video</a>',
+        url: '<a href="https://player.vimeo.com/video/132252780">Video</a><a href="https://www.vimeo.com/video/132252780">Video</a>',
         result: [
             {
                 id: "132252780",
--- test/get.js
+++ test/get.js
@@ -1,1 +1,87 @@
 "use strict";
+
+const chai = require('chai');
+const chaiAsPromised = require("chai-as-promised");
+const oEmbed = require('../main');
+const InvalidArgumentError = oEmbed.InvalidArgumentError;
+
+const expect = chai.expect;
+const should = chai.should;
+chai.use(chaiAsPromised);
+
+describe('Get', function() {
+
+    it('should return InvalidArgumentError if url is null', function () {
+
+        const url = null;
+        return expect(oEmbed.get(url)).to.be.rejectedWith(InvalidArgumentError);
+    });
+
+    it('should return InvalidArgumentError if url is undefined', function () {
+
+        const url = undefined;
+        return expect(oEmbed.get(url)).to.be.rejectedWith(InvalidArgumentError);
+    });
+
+    it('should return InvalidArgumentError if url is number', function () {
+
+        const url = 123;
+        return expect(oEmbed.get(url)).to.be.rejectedWith(InvalidArgumentError);
+    });
+
+    it('should return InvalidArgumentError if url is object', function () {
+
+        const url = {};
+        return expect(oEmbed.get(url)).to.be.rejectedWith(InvalidArgumentError);
+    });
+
+    it('should return InvalidArgumentError if url is array', function () {
+
+        const url = [];
+        return expect(oEmbed.get(url)).to.be.rejectedWith(InvalidArgumentError);
+    });
+
+    it('should return InvalidArgumentError if url is not absolute', function () {
+
+        const url = "/embed/iOf7CsxmFCt";
+        return expect(oEmbed.get(url)).to.be.rejectedWith(InvalidArgumentError);
+    });
+
+    it('should return InvalidArgumentError if url is malformed', function () {
+
+        const url = "http://embed/iOf7CsxmFCt";
+        return expect(oEmbed.get(url)).to.be.rejectedWith(InvalidArgumentError);
+    });
+
+    it('should return result if url is valid', function () {
+
+        const url = "http://www.youtube.com/embed/iOf7CsxmFCt";
+        return expect(oEmbed.get(url)).to.eventually.be.fulfilled;
+    });
+});
+
+// Iterate through all plugins
+for (let plugin of oEmbed.plugins) {
+
+    if (plugin.tests && plugin.tests.length) {
+
+        describe('Plugin - ' + plugin.name, function () {
+
+            // Iterate through all tests defined for plugin
+            for (let i = 0; i < plugin.tests.length; i++) {
+
+                const test = plugin.tests[i];
+                const numTest = i + 1;
+
+                it('should pass test ' + numTest, function () {
+
+                    return expect(oEmbed.get(test.url, test.options))
+                        .to.eventually.be.fulfilled
+                        .then(function (result) {
+                            expect(result).to.deep.equal(test.result);
+                        });
+                });
+            }
+        });
+    }
+}
