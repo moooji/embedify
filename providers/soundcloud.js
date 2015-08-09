c95655e8ef74d9ce5905cf993cbc4152f4dd8d1a
moooji <steffen.straetz@gmail.com>
2015-08-09 22:19:31 +0200

    Added soundcloud provider

--- providers/soundcloud.js
+++ providers/soundcloud.js
@@ -7,21 +7,21 @@ const apiUrl = "http://soundcloud.com/oembed";
 
 const regExp = [
     /(https?:\/\/(soundcloud.com|snd.sc)\/(.*))/i,
-    /https?:\/\/.*\.soundcloud\.com\/player\/\?url=(https?%3A\/\/.*\.soundcloud\.com\/tracks\/.*)&amp/i
+    /https?:\/\/.+\.soundcloud\.com\/player\/\?url=(https?%3A\/\/.+\.soundcloud\.com\/tracks\/.+?)&/i
 ];
 
 const transform = function (match) {
 
     let result = match[1];
     result = result.replace("%3A//", "://");
-
+    console.log
     return result;
 };
 
 const soundcloud = provider(name, apiUrl, regExp, transform);
 
-soundcloud.addTest('https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/217998348&amp;auto_play=false',
-    'https://api.soundcloud.com/tracks/217998348');
+soundcloud.addTest('https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/217027580&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&visual=true',
+    'https://api.soundcloud.com/tracks/217027580');
 soundcloud.addTest('https://soundcloud.com/zedsdead/zeds-dead-twin-shadow-lost-you-feat-dangelo-lacy',
     'https://soundcloud.com/zedsdead/zeds-dead-twin-shadow-lost-you-feat-dangelo-lacy');
 soundcloud.addTest('http://soundcloud.com/zedsdead/zeds-dead-twin-shadow-lost-you-feat-dangelo-lacy',
