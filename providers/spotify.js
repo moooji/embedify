2d3c1d100adaa3718e21a5823f224c7f5ef39167
moooji <steffen.straetz@gmail.com>
2015-08-09 19:52:52 +0200

    Added spotify provider

--- providers/spotify.js
+++ providers/spotify.js
@@ -8,7 +8,8 @@ const apiUrl = "https://embed.spotify.com/oembed/";
 const regExp = [
     /https?:\/\/open\.spotify\.com\/(.*)\/(.*)/i,
     /https?:\/\/play\.spotify\.com\/(.*)\/(.*)/i,
-    /https?:\/\/embed\.spotify\.com\/\?uri=spotify:(.*):(.*)/i
+    /https?:\/\/embed\.spotify\.com\/\?uri=spotify:(.*):(.*)/i,
+    /https?:\/\/embed\.spotify\.com\/\?uri=spotify%3A(.*)%3A(.*)/i
 ];
 
 const transform = function (match) {
@@ -18,6 +19,7 @@ const transform = function (match) {
 const spotify = provider(name, apiUrl, regExp, transform);
 
 spotify.addTest('https://embed.spotify.com/?uri=spotify:track:4th1RQAelzqgY7wL53UGQt', 'https://open.spotify.com/track/4th1RQAelzqgY7wL53UGQt');
+spotify.addTest('https://embed.spotify.com/?uri=spotify%3Atrack%3A4th1RQAelzqgY7wL53UGQt', 'https://open.spotify.com/track/4th1RQAelzqgY7wL53UGQt');
 spotify.addTest('https://open.spotify.com/track/4th1RQAelzqgY7wL53UGQt', 'https://open.spotify.com/track/4th1RQAelzqgY7wL53UGQt');
 spotify.addTest('https://play.spotify.com/track/4th1RQAelzqgY7wL53UGQt', 'https://open.spotify.com/track/4th1RQAelzqgY7wL53UGQt');
 spotify.addTest('http://open.spotify.com/track/4th1RQAelzqgY7wL53UGQt', 'https://open.spotify.com/track/4th1RQAelzqgY7wL53UGQt');
