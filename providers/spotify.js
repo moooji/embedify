3bbf14e47dbcc978e990a210fa2e42c5ce82a9f6
moooji <steffen.straetz@gmail.com>
2015-08-09 20:01:41 +0200

    Added spotify provider

--- providers/spotify.js
+++ providers/spotify.js
@@ -19,7 +19,7 @@ const transform = function (match) {
 const spotify = provider(name, apiUrl, regExp, transform);
 
 spotify.addTest('https://embed.spotify.com/?uri=spotify:track:4th1RQAelzqgY7wL53UGQt', 'https://open.spotify.com/track/4th1RQAelzqgY7wL53UGQt');
-spotify.addTest('https://embed.spotify.com/?uri=spotify%3Atrack%3A4th1RQAelzqgY7wL53UGQt', 'https://open.spotify.com/track/4th1RQAelzqgY7wL53UGQt');
+spotify.addTest('https://embed.spotify.com/?uri=spotify%3Atrack%3A6hUScKZXCeLfAhFKnLUdhI', 'https://open.spotify.com/track/6hUScKZXCeLfAhFKnLUdhI');
 spotify.addTest('https://open.spotify.com/track/4th1RQAelzqgY7wL53UGQt', 'https://open.spotify.com/track/4th1RQAelzqgY7wL53UGQt');
 spotify.addTest('https://play.spotify.com/track/4th1RQAelzqgY7wL53UGQt', 'https://open.spotify.com/track/4th1RQAelzqgY7wL53UGQt');
 spotify.addTest('http://open.spotify.com/track/4th1RQAelzqgY7wL53UGQt', 'https://open.spotify.com/track/4th1RQAelzqgY7wL53UGQt');
